50ae9a5d84da627d916de422b9e97232bde61153
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-29 19:09:18 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,19 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
 
 end
