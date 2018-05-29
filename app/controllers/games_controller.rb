90a3eaf0e77320bfdaecfd605ec20efb2be7637e
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-29 19:32:09 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,8 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-
+    game = Game.create(game_params)
+    render json: game
   end
 
   def index
